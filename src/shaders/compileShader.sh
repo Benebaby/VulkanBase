71d4024849e20acdf0d78a83dcc9e44fe20a5e64
Benebaby <benedikt.krieger@outlook.com>
2021-11-06 14:29:06 +0100

    =added shader runtime compiler

--- src/shaders/compileShader.sh
+++ src/shaders/compileShader.sh
@@ -1,2 +1,2 @@
-glslangValidator  -V shader.frag -o frag.spv --target-env
-glslangValidator -V shader.vert -o vert.spv --target-env
+glslangValidator  -V shader.frag -o compiled/frag.spv --target-env
+glslangValidator -V shader.vert -o compiled/vert.spv --target-env
